01dfa67398b3687702729e0ef67af0003bb6b7dc
Evan Dunkel <43302291+evan-dunkel@users.noreply.github.com>
2026-04-25 22:48:03 -0700

    Update sanity-image.ts

--- src/lib/sanity-image.ts
+++ src/lib/sanity-image.ts
@@ -1,6 +1,6 @@
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url";
 import { sanityClient } from "sanity:client";
+import type { SanityImageSource } from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(sanityClient);
 
